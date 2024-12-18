6e4b2c3db69757078b29a7a632a3b8e7ab877dff
mohamed591195 <mohamedsamir591195@gmail.com>
2024-12-18 02:20:06 +0200

    Update 918-Maximum-Sum-Circular-Subarray.cpp
    added another syntax during revision

--- leetcode-medium/918-Maximum-Sum-Circular-Subarray.cpp
+++ leetcode-medium/918-Maximum-Sum-Circular-Subarray.cpp
@@ -24,3 +24,29 @@ public:
         return max(maxSum, arraySum-minSum);
     }
 };
+
+// another way of writing the solution using kadanes algorithm
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int currMin = nums[0], currMax = nums[0];
+        int minSum = nums[0], maxSum = nums[0];
+        int totalSum = nums[0];
+
+        for(int i=1; i<nums.size(); ++i) {
+            if (currMax > 0) currMax += nums[i];
+            else currMax = nums[i];
+            maxSum = max(currMax, maxSum);
+
+            if (currMin < 0) currMin += nums[i];
+            else currMin = nums[i];
+            minSum = min(currMin, minSum); 
+
+            totalSum += nums[i]; 
+        }
+        
+        if (maxSum < 0) return maxSum;
+        if (totalSum - minSum > maxSum) return totalSum - minSum;
+        return maxSum;
+    }
+};
