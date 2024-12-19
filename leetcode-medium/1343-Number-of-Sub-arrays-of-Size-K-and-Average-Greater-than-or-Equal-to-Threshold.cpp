22192067d2bfdf89260afdd3137d0278971d5a6c
mohamed591195 <mohamedsamir591195@gmail.com>
2024-12-19 02:53:51 +0200

    Update 1343-Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold.cpp
    updating the solution with an enhanced syntax

--- leetcode-medium/1343-Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold.cpp
+++ leetcode-medium/1343-Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold.cpp
@@ -1,24 +1,46 @@
+// class Solution {
+// public:
+//     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+//         int L=0;
+//         int result = 0;
+//         // double currSum = 0;
+//         int currSum = 0;
+
+//         int R = 0;
+//         while ((R - L) < (k -1)) {
+//             currSum += arr[R];
+//             R++;
+//         }
+
+//         for (R; R<arr.size(); ++R){
+//             currSum += arr[R];
+//             // if (currSum/(double)k >= threshold) result++;
+//             if (currSum/k >= threshold) result++;
+//             currSum -= arr[L];
+//             L++;
+//         }
+//         return result;
+//     }
+// };
+
+// if the problem isn't about subarrays (for example subsequence, or subsets) we would use a recursive solution
+// to get all the possible combinations that satisfy the condition 
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
-        int L=0;
-        int result = 0;
-        // double currSum = 0;
-        int currSum = 0;
-
-        int R = 0;
-        while ((R - L) < (k -1)) {
-            currSum += arr[R];
-            R++;
+        int currWindowSum = 0;
+        int L = 0, R=0;
+        int counter = 0;
+        while(R-L < k && R<arr.size()) {
+            currWindowSum += arr[R++];
         }
+        if (currWindowSum / k >= threshold) counter++;
 
         for (R; R<arr.size(); ++R){
-            currSum += arr[R];
-            // if (currSum/(double)k >= threshold) result++;
-            if (currSum/k >= threshold) result++;
-            currSum -= arr[L];
-            L++;
+            currWindowSum -= arr[L++];
+            currWindowSum += arr[R];
+            if (currWindowSum / k >= threshold) counter++;
         }
-        return result;
+        return counter;
     }
 };
