792859273093847b96dec0af2d9f8ae030f69fc0
mohamed591195 <mohamedsamir591195@gmail.com>
2024-12-26 04:31:14 +0200

    Update 134-Gas-Station.cpp

--- leetcode-medium/134-Gas-Station.cpp
+++ leetcode-medium/134-Gas-Station.cpp
@@ -22,3 +22,23 @@ public:
         return -1;
     }
 };
+
+// another popular solution that also utilize the greedy strategy 
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int totalSurplus = 0;
+        int tank = 0;
+        int start = 0;
+        for (int i=0; i<n; ++i) {
+            totalSurplus += gas[i] - cost[i];
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                tank = 0;
+                start = i + 1;
+            }
+        }
+        return (totalSurplus < 0) ? -1 : start;
+    }
+};
