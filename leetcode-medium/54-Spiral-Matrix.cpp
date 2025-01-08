a3526685b601b4da1b7655e5d0aad7c617420095
mohamed591195 <mohamedsamir591195@gmail.com>
2025-01-08 13:37:05 +0200

    Update 54-Spiral-Matrix.cpp

--- leetcode-medium/54-Spiral-Matrix.cpp
+++ leetcode-medium/54-Spiral-Matrix.cpp
@@ -42,3 +42,48 @@ public:
         return result;
     }
 };
+
+
+// An Enhaned version 
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+        int m = matrix.size(), n = matrix[0].size();
+        
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse from Left to Right
+            for (int j = left; j <= right; ++j) {
+                result.push_back(matrix[top][j]);
+            }
+            top++;
+
+            // Traverse from Top to Bottom
+            for (int i = top; i <= bottom; ++i) {
+                result.push_back(matrix[i][right]);
+            }
+            right--;
+
+            // Traverse from Right to Left
+            if (top <= bottom) {
+                for (int j = right; j >= left; --j) {
+                    result.push_back(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+
+            // Traverse from Bottom to Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; --i) {
+                    result.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+};
